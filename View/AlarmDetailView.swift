14aa73aff20b15087e1daee813c5e0e0c48decfb
Christopher Kong <xism360@tamu.edu>
2023-03-26 17:40:16 -0500

    Added code functionality for AlarmDetailView, users can edit existing alarms and the UI for creating new alarms is added.

--- View/AlarmDetailView.swift
+++ View/AlarmDetailView.swift
@@ -1,22 +1,60 @@
-//
-//  AlarmDetailView.swift
-//  Alarmer Test
-//
-//  Created by user234729 on 3/26/23.
-//
-
 import SwiftUI
 
 struct AlarmDetailView: View {
-    var completion: (Alarm) -> Void
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var alarm: Alarm
+    var onSave: ((Alarm) -> Void)?
+    
+    @State private var selectedTime: Date
+    @State private var message: String
+    @State private var phoneNumber: String
+    @State private var isEnabled: Bool
+    @State private var isRecurring: Bool
+    @State private var isLocationBased: Bool
+    
+    init(alarm: Binding<Alarm>, onSave: ((Alarm) -> Void)? = nil) {
+        _alarm = alarm
+        _selectedTime = State(initialValue: alarm.wrappedValue.time)
+        _message = State(initialValue: alarm.wrappedValue.message)
+        _phoneNumber = State(initialValue: alarm.wrappedValue.phoneNumber)
+        _isEnabled = State(initialValue: alarm.wrappedValue.isEnabled)
+        _isRecurring = State(initialValue: alarm.wrappedValue.isRecurring)
+        _isLocationBased = State(initialValue: false)
+        self.onSave = onSave
+    }
     
     var body: some View {
-        Text("Alarm Detail View")
+        NavigationView {
+            Form {
+                DatePicker("Time", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                TextField("Message", text: $message)
+                TextField("Phone Number", text: $phoneNumber)
+                    .keyboardType(.phonePad)
+                Toggle("Enabled", isOn: $isEnabled)
+                Toggle("Recurring", isOn: $isRecurring)
+                Toggle("Location Based", isOn: $isLocationBased)
+            }
+            .navigationTitle("Alarm Details")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        let updatedAlarm = Alarm(id: alarm.id, time: selectedTime, message: message, phoneNumber: phoneNumber, isEnabled: isEnabled, isRecurring: isRecurring)
+                        onSave?(updatedAlarm)
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct AlarmDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        AlarmDetailView { _ in }
+        AlarmDetailView(alarm: .constant(Alarm(time: Date(), message: "Wake up!", phoneNumber: "+1234567890", isEnabled: true, isRecurring: true)), onSave: { _ in })
     }
 }
--- View/AlarmListView.swift
+++ View/AlarmListView.swift
@@ -1,10 +1,3 @@
-//
-//  AlarmListView.swift
-//  Alarmer Test
-//
-//  Created by user234729 on 3/26/23.
-//
-
 import SwiftUI
 
 struct Alarm: Identifiable {
@@ -13,99 +6,121 @@ struct Alarm: Identifiable {
     var message: String
     var phoneNumber: String
     var isEnabled: Bool
-    var isRecurring: Bool}
+    var isRecurring: Bool
+}
+
+struct Formatters {
+    static let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
 
 struct AlarmListView: View {
     @State private var alarms: [Alarm] = [
         // Example alarms
         Alarm(time: Date(), message: "Wake up!", phoneNumber: "+1234567890", isEnabled: true, isRecurring: true),
-        Alarm(time: Date().addingTimeInterval(3600), message: "Get ready for work", phoneNumber: "+0987654321", isEnabled: false, isRecurring: false)    ]
+        Alarm(time: Date().addingTimeInterval(3600), message: "Get ready for work", phoneNumber: "+0987654321", isEnabled: false, isRecurring: false)
+    ]
     
     @State private var isPresentingAlarmDetailView = false
     
     var body: some View {
         NavigationView {
             ScrollView {
-                           VStack(spacing: 40) { // Adjust spacing as needed
-                               ForEach($alarms) { alarm in
-                                   AlarmRow(alarm: alarm)
-                               }
-                           }
-                           .padding(.vertical, 60) // Add this line for top and bottom padding
-                       }.navigationTitle("Alarmers")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        isPresentingAlarmDetailView.toggle()
-                    }) {
-                        Image(systemName: "plus")
+                VStack(spacing: 0) { // Adjust spacing as needed
+                    ForEach(alarms.indices, id: \.self) { index in
+                        NavigationLink(destination: AlarmDetailView(alarm: $alarms[index], onSave: { updatedAlarm in
+                            updateAlarm(updatedAlarm)
+                        })) {
+                            AlarmRow(alarm: $alarms[index], onToggle: {
+                                toggleAlarmEnabled(index)
+                            })
+                        }
+                    }
+                    .padding(.vertical, 20) // Add this line for top and bottom padding
+                }
+                .navigationTitle("Alarmers")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            isPresentingAlarmDetailView.toggle()
+                        }) {
+                            Image(systemName: "plus")
+                        }
                     }
                 }
-            }
-            .sheet(isPresented: $isPresentingAlarmDetailView) {
-                AlarmDetailView { alarm in
-                    addAlarm(alarm)
-                    isPresentingAlarmDetailView.toggle()
+                .sheet(isPresented: $isPresentingAlarmDetailView) {
+                    AlarmDetailView(alarm: .constant(Alarm(time: Date(), message: "", phoneNumber: "", isEnabled: false, isRecurring: false)), onSave: { newAlarm in
+                        alarms.append(newAlarm)
+                        isPresentingAlarmDetailView.toggle()
+                    })
                 }
             }
         }
-    }
-    
-    func addAlarm(_ alarm: Alarm) {
-        alarms.append(alarm)
-    }
-    
-    func deleteAlarm(at offsets: IndexSet) {
-        alarms.remove(atOffsets: offsets)
-    }
-}
 
-struct AlarmRow: View {
-    @Binding var alarm: Alarm
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: 20)
-            .fill(alarm.isEnabled
-                ? Color(red: 1, green: 0.82, blue: 0.34)
-                : Color(red: 0.89, green: 0.87, blue: 0.84)).frame(width: 326, height: 114).shadow(radius: 4, y: 4)
-            .overlay(
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text(alarm.isRecurring ? "Recurring" : "Not Recurring")
-                            .font(.subheadline)
-                            .foregroundColor(alarm.isEnabled ? Color.black : Color(red: 0.44, green: 0.43, blue: 0.43))
-                                             
-                        Text(alarm.time, style: .time)
-                            .font(.system(size: 34)) // Adjust font size as needed
-                            .fontWeight(.bold)
-                            .foregroundColor(alarm.isEnabled ? Color.black : Color(red: 0.44, green: 0.43, blue: 0.43))
-                    }
-                    Spacer()
-                    
-                    Button(action: {
-                        withAnimation {
-                            alarm.isEnabled.toggle()
-                        }
-                    }){
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 25)
-                                .frame(width: 83, height: 41)
-                                .foregroundColor(alarm.isEnabled ? Color(red: 1, green: 0.72, blue: 0) : .gray)
-                            
-                            Circle()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.white)
-                                .offset(x: alarm.isEnabled ? 20 : -20)
-                        }
-                    }
-                    .buttonStyle(BorderlessButtonStyle())
-                }
-                .padding()
-            )
+        
     }
-}
-struct AlarmListView_Previews: PreviewProvider {
-    static var previews: some View {
-        AlarmListView()
+    
+    struct AlarmRow: View {
+        @Binding var alarm: Alarm
+        var onToggle: () -> Void
+        
+        var body: some View {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(alarm.isEnabled
+                      ? Color(red: 1, green: 0.82, blue: 0.34)
+                      : Color(red: 0.89, green: 0.87, blue: 0.84)).frame(width: 326, height: 114).shadow(radius: 4, y: 4)
+                .overlay(
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(alarm.isRecurring ? "Recurring" : "Not Recurring")
+                                .font(.subheadline)
+                                .foregroundColor(alarm.isEnabled ? Color.black : Color(red: 0.44, green: 0.43, blue: 0.43))
+                            
+                            Text(Formatters.timeFormatter.string(from: alarm.time))
+                                .font(.system(size: 34))
+                                .fontWeight(.bold)
+                                .foregroundColor(alarm.isEnabled ? Color.black : Color(red: 0.44, green: 0.43, blue: 0.43))
+                        }
+                        Spacer()
+                        
+                        Button(action: {
+                            withAnimation {
+                                onToggle()
+                            }
+                        }) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .frame(width: 83, height: 41)
+                                    .foregroundColor(alarm.isEnabled ? Color(red: 1, green: 0.72, blue: 0) : .gray)
+                                
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.white)
+                                    .offset(x: alarm.isEnabled ? 20 : -20)
+                            }
+                        }
+                        .buttonStyle(BorderlessButtonStyle())
+                    }
+                        .padding()
+                )
+        }
+    }
+    
+    func toggleAlarmEnabled(_ index: Int) {
+        alarms[index].isEnabled.toggle()
+    }
+    
+    func updateAlarm(_ updatedAlarm: Alarm) {
+        if let index = alarms.firstIndex(where: { $0.id == updatedAlarm.id }) {
+            alarms[index] = updatedAlarm
+        }
+    }
+    struct AlarmListView_Previews: PreviewProvider {
+        static var previews: some View {
+            AlarmListView()
+        }
     }
 }
